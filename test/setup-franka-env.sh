af0c05da0319522dfca3b85089cadf2d7f9840f6
Julian Theis <jlntheis@gmail.com>
2018-11-01 12:14:01 +0100

    Fix paths for lab computer

--- test/setup-franka-env.sh
+++ test/setup-franka-env.sh
@@ -1,5 +1,9 @@
 # Lazy script to set up environment for testing.
 # You probably have to adjust the values for use on your own machine.
 
-export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:/code/rtt-franka-hardware-integration/build/orocos:/code/rtt-franka-hardware-integration/test/robot_data_test/build/src/orocos
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/code/cogimon-gazebo-models
+source ~/citk/systems/cogimon-minimal-nightly/bin/setup-cogimon-env.sh
+
+export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:~/rtt-franka-hardware-integration/build/orocos:~/rtt-franka-hardware-integration/test/robot_data_test/build/src/orocos
+export GAZEBO_MODEL_PATH=~/cogimon-gazebo-models
+
+export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:~/franka-test/JointPositionControl/build/src/orocos
