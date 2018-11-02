b4732ed28fe7810c9b0fb22be4ef9dd7bbbcc175
Julian Theis <jlntheis@gmail.com>
2018-11-02 13:25:12 +0100

    Unset $PYTHONPATH
    Otherwise debugging with GDB/Qt Creator won't work properly

--- test/setup-franka-env.sh
+++ test/setup-franka-env.sh
@@ -2,6 +2,7 @@
 # You probably have to adjust the values for use on your own machine.
 
 source ~/citk/systems/cogimon-minimal-nightly/bin/setup-cogimon-env.sh
+unset PYTHONPATH
 
 export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:~/rtt-franka-hardware-integration/build/orocos:~/rtt-franka-hardware-integration/test/robot_data_test/build/src/orocos
 export GAZEBO_MODEL_PATH=~/cogimon-gazebo-models
