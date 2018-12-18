ff3a1a16a95ae9430d9f65571dc8c05ddf58634d
Julian Theis <jlntheis@gmail.com>
2018-12-18 15:45:00 +0100

    Update setup script

--- test/setup-franka-env.sh
+++ test/setup-franka-env.sh
@@ -4,7 +4,4 @@
 source ~/citk/systems/cogimon-minimal-nightly/bin/setup-cogimon-env.sh
 unset PYTHONPATH
 
-export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:~/rtt-franka-hardware-integration/build/orocos:~/rtt-franka-hardware-integration/test/robot_data_test/build/src/orocos
-export GAZEBO_MODEL_PATH=~/cogimon-gazebo-models
-
-export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:~/franka-test/JointPositionControl/build/src/orocos
+export RTT_COMPONENT_PATH=$RTT_COMPONENT_PATH:~/code/rtt-franka-hardware-integration/build/orocos:~/code/rtt-franka-hardware-integration/test/robot_data_test/build/src/orocos
